5f6bf83f78b629bb3199f09fc0bbeed571363020
Fatihbykl <fatih.baykal54@hotmail.com>
2025-02-26 13:49:59 +0300

    updated app.py

--- app.py
+++ app.py
@@ -5,17 +5,6 @@ import os
 
 app = Flask(__name__)
 
-def scheduled_update():
-    print("Updating...")
-    update_opportunities()
-    print("Update is over.")
-
-# Creates background task for executing update function with interval
-if not app.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
-  sched = BackgroundScheduler()
-  sched.add_job(scheduled_update, "interval", minutes=1)
-  sched.start()
-
 @app.route("/", methods=["GET"])
 def serve_xml():
     """ Returns XML file """
